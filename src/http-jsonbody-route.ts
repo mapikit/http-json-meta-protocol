c3cd07853ba1c1c739915aaf8ca670cebcff23e0
Fábio Meneses Junior <flgmjr@gmail.com>
2021-08-15 19:29:01 -0300

    Code migration from meta-system

---
+++ src/http-jsonbody-route.ts
@@ -0,0 +1,37 @@
+import { Request, Response, Router } from "express";
+import { FunctionManager } from "meta-protocol-helper/dist/src/meta-protocol";
+import { HTTPRouteConfiguration } from "./configuration";
+import { HTTPJsonBodyInputMap } from "./input-map";
+import { HTTPJsonBodyOutputMap } from "./output-map";
+
+export class HTTPJsonBodyRoute {
+  public constructor (
+    private routeConfigurations : HTTPRouteConfiguration,
+    private functionManager : FunctionManager,
+  ) { }
+
+  public setupRouter () : Router {
+    const router = Router();
+    const method = this.routeConfigurations.method
+      .toLocaleLowerCase() as HTTPRouteConfiguration["method"];
+
+    router[method](
+      this.routeConfigurations.route,
+      this.wrapFunctionInProtocol(),
+    );
+
+    return router;
+  }
+
+  private wrapFunctionInProtocol () : (req : Request, res : Response) => Promise<void> {
+    const bop = this.functionManager.get(this.routeConfigurations.businessOperation);
+
+    return (async (req : Request, res : Response) : Promise<void> => {
+      const functionInputs = HTTPJsonBodyInputMap.mapInputs(req, this.routeConfigurations);
+
+      const result = await bop(functionInputs);
+
+      HTTPJsonBodyOutputMap.resolveOutput(result, res, this.routeConfigurations);
+    });
+  }
+}
