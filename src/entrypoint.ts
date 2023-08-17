71c1cdf491177ed30a98b00fdd25a31a95e077ce
Fábio Meneses Junior <flgmjr@gmail.com>
2023-08-17 01:22:22 -0300

    fix entrypoint

--- src/entrypoint.ts
+++ src/entrypoint.ts
@@ -1,19 +1,18 @@
 import { HttpMetaProtocol } from "./index.js";
 import { HTTP_CONFIGURATION } from "./configuration.js";
 
-let map = new Map();
-let addonConfig;
-
 export const configure = (broker, configuration : HTTP_CONFIGURATION) => {
+  broker.done();
+  return configuration;
+}
+
+export const boot = async (broker, configuration) => {
+  const map = new Map();
+
   configuration.routes.forEach((route) => {
     map.set(route.businessOperation, broker.bopFunctions.getBopFunction(route.businessOperation))
   })
 
-  addonConfig = configuration
-  broker.done();
-}
-
-export const boot = async (broker) => {
-  const server = new HttpMetaProtocol(addonConfig as HTTP_CONFIGURATION, map);
+  const server = new HttpMetaProtocol(configuration as HTTP_CONFIGURATION, map);
   await server.start();
 }
